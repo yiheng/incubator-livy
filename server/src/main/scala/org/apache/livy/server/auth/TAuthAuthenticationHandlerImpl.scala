4ffe2957315ab2812290a3b03830bf64225a567d
yihengwang <yihengwang@tencent.com>
2020-01-02 20:12:11 +0800

    fix style check

--- server/src/main/scala/org/apache/livy/server/auth/TAuthAuthenticationHandlerImpl.scala
+++ server/src/main/scala/org/apache/livy/server/auth/TAuthAuthenticationHandlerImpl.scala
@@ -1,15 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.livy.server.auth
 
 import java.io.IOException
 import java.util.Properties
 
+import com.tencent.tdw.security.authentication.{Authentication, LocalKeyManager}
+import com.tencent.tdw.security.authentication.service.{SecureService, SecureServiceFactory}
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 import javax.servlet.ServletException
 import org.apache.hadoop.security.authentication.client.AuthenticationException
 import org.apache.hadoop.security.authentication.server.{AuthenticationHandler, AuthenticationToken}
+
 import org.apache.livy.Logging
-import com.tencent.tdw.security.authentication.{Authentication, LocalKeyManager}
-import com.tencent.tdw.security.authentication.service.{SecureService, SecureServiceFactory}
 
 class TAuthAuthenticationHandlerImpl extends AuthenticationHandler with Logging {
   private var secureService: SecureService = _
